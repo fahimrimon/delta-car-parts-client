9181ec0d029cb74cc9136e6c56e0c417833e658b
Fahim Rimon <fahimrimon21@gmail.com>
2022-05-22 15:44:01 +0600

    business summary

---
+++ src/Pages/Home/BusinessSummary.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const BusinessSummary = () => {
+  return (
+    <div className="mt-16">
+      <h2 className="text-center text-yellow-400 text-xl mb-5">Business Summary</h2>
+      <div class="stats stats-vertical lg:stats-horizontal shadow grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 text-center">
+        <div class="stat">
+          <div class="stat-title">Positive Seller Ratings</div>
+          <div class="stat-value text-primary">95%</div>
+        </div>
+
+        <div class="stat">
+          <div class="stat-title">Total Users</div>
+          <div class="stat-value text-accent">2,700</div>
+        </div>
+
+        <div class="stat">
+          <div class="stat-title">Ship On Time</div>
+          <div class="stat-value text-secondary">98%</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BusinessSummary;
