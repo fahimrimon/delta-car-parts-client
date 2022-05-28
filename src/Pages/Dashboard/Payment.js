7ab2cd58379841882e14852691c836309c509d0d
Fahim Rimon <fahimrimon21@gmail.com>
2022-05-28 21:22:57 +0600

    payment success

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,40 +1,51 @@
-import React from 'react';
-import { useQuery } from 'react-query';
-import { useParams } from 'react-router-dom';
-import Loading from '../Shared/Loading';
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import React from "react";
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import Loading from "../Shared/Loading";
+import CheckoutForm from "./CheckoutForm";
+
+const stripePromise = loadStripe(
+  "pk_test_51L3wGiCvnZufzekA4SQLnrZrwQtUIAcqEkSi8J2hh3RPRb6SNB55XYsKzOEW6AOpTDO5MptDGELmJur2sQv5sf3m00cKHi9fb1"
+);
 
 const Payment = () => {
-    const { id } = useParams();
-    const url = `http://localhost:5000/purchase/${id}`;
-    const { data: order, isLoading } = useQuery(['purchase', id], () => fetch(url, {
-        method: 'GET',
-        headers: {
-            'authorization': `Bearer ${localStorage.getItem('accessToken')}`
-        }
-    }).then(res => res.json()));
+  const { id } = useParams();
+  const url = `http://localhost:5000/purchase/${id}`;
+  const { data: order, isLoading } = useQuery(["purchase", id], () =>
+    fetch(url, {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    }).then((res) => res.json())
+  );
 
-    if (isLoading) {
-        return <Loading></Loading>
-    }
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
 
-    return (
-        <div>
-            <div class="card w-50 max-w-md bg-base-100 shadow-xl my-12">
-                <div class="card-body">
-                    <p className="text-secondary font-bold">Hello, {order.userName}</p>
-                    <h2 class="card-title">Your Product Name: <span className='text-red-400'>{order.productName}</span></h2>
-                    <p>Please pay: ${order.productPrice}</p>
-                </div>
-            </div>
-            <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
-                <div class="card-body">
-                    {/* <Elements stripe={stripePromise}>
-                        <CheckoutForm appointment={appointment} />
-                    </Elements> */}
-                </div>
-            </div>
+  return (
+    <div>
+      <div class="card w-50 max-w-md bg-base-100 shadow-xl my-12">
+        <div class="card-body">
+          <p className="text-secondary font-bold">Hello, {order.userName}</p>
+          <h2 class="card-title">
+            Your Product Name: <span className="text-red-400">{order.productName}</span>
+          </h2>
+          <p>Please pay: ${order.productPrice}</p>
         </div>
-    );
+      </div>
+      <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
+        <div class="card-body">
+          <Elements stripe={stripePromise}>
+                        <CheckoutForm order={order} />
+                    </Elements>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Payment;
