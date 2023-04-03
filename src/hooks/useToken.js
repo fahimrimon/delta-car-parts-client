2110cfa64e6b830b26e644fb308fb56b5039ef16
Fahim Rimon <67573663+fahimrimon@users.noreply.github.com>
2023-04-03 05:39:26 -1200

    useToken

--- src/hooks/useToken.js
+++ src/hooks/useToken.js
@@ -7,7 +7,7 @@ const useToken = (user) => {
     const email = user?.user?.email;
     const currentUser = { email: email };
     if (email) {
-      fetch(`https://powerful-beyond-98796.herokuapp.com/user/${email}`, {
+      fetch(`https://delta-car-parts-server.vercel.app/user/${email}`, {
         method: "PUT",
         headers: {
           "content-type": "application/json",
